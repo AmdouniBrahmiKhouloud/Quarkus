c350fb1d64fd36f9d77865cad12e4f79ec19ab87
AmdouniBrahmiKhouloud <khouloud.amdounibrahmi@esprit.tn>
2022-06-22 08:43:38 +0100

    Quarkus started project

---
+++ src/main/java/org/reativeFruits/panache/Fruit.java
@@ -0,0 +1,14 @@
+package org.reativeFruits.panache;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Cacheable
+public class Fruit extends PanacheEntity {
+    @Column(length = 40, unique = true)
+    public String name;
+}
---
+++ src/test/java/org/reactiveFruits/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.reactiveFruits;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
